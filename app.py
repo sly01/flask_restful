74e2e2197e6c1b881893682f2843500002ddc10c
sly01 <aerkoc01@gmail.com>
2019-10-27 11:29:34 +0100

    Added delete endpoint

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
