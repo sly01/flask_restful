3025e4a5f9b30bf8b98419e5abe30161a4920297
sly01 <aerkoc01@gmail.com>
2019-10-27 11:23:03 +0100

    authentication and logging added

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'aerkoc'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # for item in items:
         #     if item['name'] == name:
